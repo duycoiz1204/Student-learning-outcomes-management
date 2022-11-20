7c9dc00135b25e1a4b9806d9286b66f1f9172f27
duycoiz1204 <duycoiz1204@gmail.com>
2022-11-20 22:17:00 +0700

    Create & design rpLstDiemOfSinhVien

---
+++ student_learning_outcomes_management/rpLstDiemOfSinhVien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using DevExpress.XtraReports.UI;
+
+namespace student_learning_outcomes_management
+{
+    public partial class rpLstDiemOfSinhVien : DevExpress.XtraReports.UI.XtraReport
+    {
+        public rpLstDiemOfSinhVien()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
